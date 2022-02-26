917af28334cca038b9592bdbc0b2179bb6843f2b
msshaikhd14 <mohamedrshaikh@gmail.com>
2022-02-26 16:24:57 +0400

    initial commit

---
+++ src/main/java/com/rs4u/quicknotes/QuickNotesApplication.java
@@ -0,0 +1,13 @@
+package com.rs4u.quicknotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rs4u/quicknotes/QuickNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rs4u.quicknotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
